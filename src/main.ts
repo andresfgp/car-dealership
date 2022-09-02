06bee220a07cd05f2173f34ce70bb532f2d3bc5a
Andres Garcia <andres.fgp@hotmail.com>
2022-09-02 12:49:29 -0500

    POST

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove extra DTO data
+      forbidNonWhitelisted: true, // bad request with extra DTO data
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
